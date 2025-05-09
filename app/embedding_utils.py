9dd50f3f47f1fbab571de7ceda0262ce4fff8931
Aksshai Nair <aksshai07nair@gmail.com>
2025-05-10 02:16:41 +0530

    initial

--- app/embedding_utils.py
+++ app/embedding_utils.py
@@ -6,9 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Initialize OpenAI client
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def embed_text(text: str) -> list:
     """
